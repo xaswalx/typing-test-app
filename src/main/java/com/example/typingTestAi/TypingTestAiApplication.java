d80fac379d69a5d095a5c01acbe4af391ebda323
Gaurav Aswal <gaurav.lm10@gmail.com>
2026-02-15 21:41:42 +0530

    Initial Spring Boot project structure

---
+++ src/main/java/com/example/typingTestAi/TypingTestAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.typingTestAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingTestAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingTestAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/typingTestAi/TypingTestAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.typingTestAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypingTestAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
